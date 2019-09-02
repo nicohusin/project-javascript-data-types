b160a63449ec5905ec4da218d62ccc6d02fb99fb
nicohusin <nicohusin@gmail.com>
2019-09-02 13:20:59 +0700

    first initial

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Hello World')
+// alert('Hello World')
+console.info('this is an info text')
+const name = 'Nico Husin'
+console.log(`hello ${name}`)
+// alert(`hello ${name}`)
+console.log(0)
+console.log(100)
+console.log(3.14)
+
+const age = 24
+
+console.log(`His age is ${age}`)
+
+console.log({
+  Name : 'nico husin',
+  age : 24,
+  superPower: true
+})
+
+const hero = { 
+  name: 'Nico',
+  age: 24, 
+  superPower: true }
+
+console.log (hero['age'])
+
+let message = 'hello'
+message = 1235
+
+console.log(message)
