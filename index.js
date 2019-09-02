ad81ce23c02ca9766c20354d99cb8c57409191d5
nicohusin <nicohusin@gmail.com>
2019-09-02 14:26:27 +0700

    add javascript file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 console.log('Hello World')
 // alert('Hello World')
+
 console.info('this is an info text')
+
 const name = 'Nico Husin'
+
 console.log(`hello ${name}`)
 // alert(`hello ${name}`)
+
 console.log(0)
 console.log(100)
 console.log(3.14)
@@ -23,9 +27,46 @@ const hero = {
   age: 24, 
   superPower: true }
 
-console.log (hero['age'])
+  console.log(hero)
+  console.log(hero['name'])
+  console.log(hero.age)
+  console.log(hero.superPower)
+  
+ /* alert(hero)
+  alert(hero['name'])
+  alert(hero.age)
+  alert(hero.superPower)*/
+  
 
-let message = 'hello'
-message = 1235
 
-console.log(message)
+// ARRAY
+console.log([1, 2, 3])
+console.log([0, 1, 2, 3])
+console.log(['Nico', 100, true])
+
+const person = ['Nico', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+/*alert(person)
+alert(person[0])
+alert(person[1])
+alert(person[2])*/
+
+//BOOLEAN
+
+console.log(true)
+console.log(false)
+console.log(true, false)
+
+const toggle = false
+
+console.log(`The lamp's toggle is ${toggle}`)
+
+// alert(`The lamp's toggle is ${toggle}`)
+
+
+
